817c6957f699292f8ab3815385038157faf7d246
adolfo-lang <adolfohdzr.1602@gmail.com>
2023-02-08 14:03:06 -0600

    wjijd

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,6 +5,9 @@ class TestCalculadora(unittest.TestCase):
     def test_2_mas_2(self):
         calc = calculadora()
         self.assertEqual(4, calc.sumar(2, 2))
+    def test_10_mas_2(self):
+        calc = calculadora()
+        self.assertEqual(11, calc.sumar(10, 2))
 
 if __name__ == '__main__':
     unittest.main()
