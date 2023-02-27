9585c02fecd227a83467ad16a2aa671ca19a25c3
adolfo-lang <adolfohdzr.1602@gmail.com>
2023-02-27 13:10:01 -0600

    correccion de error prueba2

--- test_calculadora.py
+++ test_calculadora.py
@@ -7,7 +7,7 @@ class TestCalculadora(unittest.TestCase):
         self.assertEqual(4, calc.sumar(2, 2))
     def test_10_mas_2(self):
         calc = calculadora()
-        self.assertEqual(11, calc.sumar(10, 2))
+        self.assertEqual(12, calc.sumar(10, 2))
 
 if __name__ == '__main__':
     unittest.main()
